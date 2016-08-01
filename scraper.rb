7bfdca71c4f3956f29dfe6962685736b0973ce81
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-08-01 21:30:13 +1000

    Initial commit, scraper working and saving to DB

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,59 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'mechanize'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+def extract_elems_from_paragraph(application_list, url)
+  #For each application in a <p> elem, we split that into children.
+  #We find our data by searching for substrings in children and selecting the next child element.
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+  record = {
+    "info_url" => url,
+    "comment_url" => "mailto:contactus@hume.vic.gov.au",
+    "council_reference" => "",
+    "address" => "",
+    "on_notice_to" => "",
+    "description" => "",
+    "date_scraped" => Date.today.to_s
+  }
+
+  elems_to_analyse = application_list.children
+  elems_to_analyse.each_with_index do |elem_to_extract, index|
+    if elem_to_extract.to_s.include? "Application Reference"
+      record["council_reference"] = elems_to_analyse[index + 1].inner_text.strip
+    elsif elem_to_extract.to_s.include? "Property Address"
+      record["address"] = "#{elems_to_analyse[index + 1].inner_text.strip}, VIC"
+    elsif elem_to_extract.to_s.include? "No decision will be made prior to"
+      day, month, year = elems_to_analyse[index + 1].inner_text.strip.split("/")
+      on_notice_to_formatted = "#{year}-#{month}-#{day}"
+      record["on_notice_to"] = on_notice_to_formatted
+    elsif elem_to_extract.to_s.include? "Proposal"
+      record["description"] = elems_to_analyse[index + 1].inner_text.strip
+    end
+  end
+
+  return record
+end
+
+def save_dev_application_data(record)
+  #Save data to db file, but don't overwrite the entries that exist already.
+  if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+    ScraperWiki.save_sqlite(['council_reference'], record)
+    puts "Saving record #{record['council_reference']}"
+  else
+    puts "Skipping already saved record #{record['council_reference']}"
+  end
+end
+
+url = "https://www.hume.vic.gov.au/Building_Planning/Planning/Applications_at_Advertising"
+agent = Mechanize.new
+page = agent.get(url)
+
+#For this LGA, all applications are on a single page, in a single div. Each is a p element
+div_with_applications = page.search('div.content_article_body')
+applications_all = div_with_applications.search('p')
+
+applications_all.each_with_index do |application, index|
+  #The first three <p> elements are not applications
+  next if index < 3
+  data_from_dev_application = extract_elems_from_paragraph(application, url)
+  save_dev_application_data(data_from_dev_application)
+end
